ec16bd64d3906542962a6c242174711fe1579683
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-03 17:19:35 +0300

    Add files via upload

---
+++ 169 Majority Element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+# Boyer–Moore string-search algorithm
+        count = 0
+        for i in range(len(nums)):
+            if count == 0:
+                maj_el = nums[i]
+                count = 1
+            elif maj_el == nums[i]:
+                count += 1
+            else:
+                count -=1
+        return maj_el
+
