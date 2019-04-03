842e113c881ab3b7aab5dd6ebcfd2da08e1011c2
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-03 17:11:44 +0300

    Add files via upload

---
+++ 852 Peak Index in a Mountain Array.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 17:07:21 2019
+
+@author: macbookpro
+"""
+
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+# First algorithmic solution based on QuickSort algorithm with
+# Hoare partition
+        arr = A.copy()
+        import sys
+        sys.setrecursionlimit(15000)
+
+        # Hoare partition
+        def Partition(A, p, r):
+            x = A[p]
+           i = p - 1
+           j = r + 1
+           while True:
+               while True:
+                   j = j - 1
+                   if A[j] <= x:
+                       break
+               while True:
+                   i = i + 1
+                   if A[i] >= x:
+                       break
+               if i < j:
+                   A[i], A[j] = A[j], A[i]
+               else:
+                   return j
+        
+       def QuickSort(A,p,r):
+           if p < r:
+               q = Partition(A,p,r)
+               QuickSort(A,p,q)
+               QuickSort(A,q+1,r)
+        
+        # apply sorting
+        QuickSort(arr,0,len(arr)-1)
+        max_el = arr[-1]
+        
+        idx = 0
+        for i in range(len(arr)):
+            if A[i] == max_el:
+                idx = i
+        return idx
+
+# Second solution (straight forward)    
+        k = 0
+        for i in range(1,len(A)):
+            if A[k] < A[i]:
+                k = i
+        return k
