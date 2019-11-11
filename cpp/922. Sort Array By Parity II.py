e10de0626fa7e88071dd16c922204558e2693c6a
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-11 13:07:06 +0300

    Update 922. Sort Array By Parity II.py

--- cpp/922. Sort Array By Parity II.py
+++ cpp/922. Sort Array By Parity II.py
@@ -16,3 +16,28 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& a) {
+        int even = (int)a.size() - 2;
+        int odd = (int)a.size() - 1;
+        while (true) {
+            while (even >= 0 && (a[even] & 1) == 0) {
+                even -= 2;
+            }
+            while (odd >= 0 && (a[odd] & 1) == 1) {
+                odd -= 2;
+            }
+            if (even >= 0) {
+                swap(a[even], a[odd]);
+                even -= 2;
+                odd -= 2;
+            } else {
+                break;
+            }
+        }
+        return move(a);
+    }
+};
