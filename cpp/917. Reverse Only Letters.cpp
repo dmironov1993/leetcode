487ab248154924fef45c46c09f04bc9871dd5090
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-11 15:12:13 +0300

    Update 917. Reverse Only Letters.cpp

--- cpp/917. Reverse Only Letters.cpp
+++ cpp/917. Reverse Only Letters.cpp
@@ -24,6 +24,39 @@ public:
 };
 
 
+
+//Second solution
+class Solution {
+public:
+    
+    bool isLetter(char c) {
+        return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+    }
+    
+    string reverseOnlyLetters(string s) {
+        int i = 0;
+        int j = (int)s.size() - 1;
+        while (true) {
+            while (i < j and not (isLetter(s[i]))) {
+                i++;
+            }
+            while (i < j and not (isLetter(s[j]))) {
+                j--;
+            }
+            if (i < j) {
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            } else {
+                break;
+            }
+        }
+        return move(s);
+    }
+};
+
+
+
 //My solution
 class Solution {
 public:
