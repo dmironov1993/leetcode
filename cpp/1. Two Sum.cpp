26c484fdd35a696c02cdeeb543d0ea7b765139d5
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-16 14:22:24 +0300

    Update 1. Two Sum.cpp

--- cpp/1. Two Sum.cpp
+++ cpp/1. Two Sum.cpp
@@ -1,3 +1,26 @@
+//Second solution
+class Solution {
+    
+    bool canSubtract(int a, int b) {
+        return (b >= 0 && a >= INT_MIN + b) || (b < 0 && a - b <= INT_MAX + b);
+    }
+    
+public:
+    vector<int> twoSum(const vector<int>& a, int target) {
+        unordered_map<int, int> valueToIndex;
+        for (int i = 0; i < (int)a.size(); i++) {
+            if (canSubtract(target, a[i])) {
+                auto it = valueToIndex.find(target-a[i]);
+                if (it != valueToIndex.end()) {
+                    return {it->second, i};
+                }
+            }
+            valueToIndex[a[i]] = i;
+        }
+        throw invalid_argument("sum not found");
+    }
+};
+
 //First solution
 class Solution {
 public:
