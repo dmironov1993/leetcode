9f7a78c9e0c02fb9cf3b9512702ba822d7cd9f3d
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-16 19:26:47 +0300

    Create 20. Valid Parentheses.py

---
+++ cpp/20. Valid Parentheses.py
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(const string &s) {
+        stack<char, vector<char>> st;
+        for (char c: s) {
+            if (c=='(') {
+                st.push(')');
+            } else if (c == '[') {
+                st.push(']');
+            } else if (c=='{') {
+                st.push('}');
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                if (st.top() != c) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
