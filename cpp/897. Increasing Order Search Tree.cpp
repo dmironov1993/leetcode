165b00c9f62905b36659a18cf4dbb41f29def367
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-16 13:26:03 +0300

    Update 897. Increasing Order Search Tree.cpp

--- cpp/897. Increasing Order Search Tree.cpp
+++ cpp/897. Increasing Order Search Tree.cpp
@@ -1,3 +1,4 @@
+//First solution
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -26,5 +27,53 @@ public:
         TreeNode * result = nullptr;
         visit(root, result);
         return result;
+    }
+};
+
+
+
+//Second solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* current = root;
+        TreeNode* parent = nullptr;
+        TreeNode* result = nullptr;
+        while (true) {
+            if (current == nullptr) {
+                if (parent == nullptr) {
+                    return result;
+                } else {
+                    assert(parent != nullptr);
+                    current = parent;
+                    parent = parent -> right;
+                    current -> right = nullptr;
+                }
+            } else {
+                assert(current != nullptr);
+                if (current -> right != nullptr) {
+                    TreeNode *tmp = current -> right;
+                    current -> right = parent;
+                    parent = current;
+                    current = tmp;
+                } else {
+                    assert(current-> right == nullptr);
+                    current -> right = result;
+                    result = current;
+                    current = current -> left;
+                    result -> left = nullptr;
+                }
+            }
+        }
     }
 };
