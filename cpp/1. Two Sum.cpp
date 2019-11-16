79b681689a3e48470215713d462e3ecae0a1b368
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-16 17:54:29 +0300

    Update 1. Two Sum.cpp

--- cpp/1. Two Sum.cpp
+++ cpp/1. Two Sum.cpp
@@ -36,3 +36,22 @@ public:
         throw invalid_argument("sum not found");
     }
 };
+
+
+//Third O(n^2) solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& a, int target) {
+        vector<int> ans;
+        for (int i = 0; i < (int)a.size(); i++) {
+            for (int j = i+1; j < (int)a.size(); j++) {
+                if (a[i] + a[j] == target) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
