82a449111d43587990c19e265eb1dc6879d23e2b
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-12-03 12:17:35 +0300

    Update 268 Missing Number.cpp

--- cpp/268 Missing Number.cpp
+++ cpp/268 Missing Number.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/missing-number/
 
+//First solution when sum can be represented as int type
 class Solution {
 public:
     int missingNumber(const vector<int>& nums) {
@@ -9,6 +10,25 @@ public:
         }
         int n = (int)nums.size();
         int res = n*(n+1) / 2 - s;
+        if (res == 0) {
+            return 0;
+        } else {
+            return res;
+        }
+    }
+};
+
+
+//Second solution which is very similar to the first one but sum is represented as long long type this time
+class Solution {
+public:
+    int missingNumber(const vector<int>& nums) {
+        long long s = 0;
+        for (int i = 0; i < (int)nums.size(); i++) {
+            s += nums[i];
+        }
+        long long n = (int)nums.size();
+        long long res = n*(n+1) / 2 - s;
         if (res == 0) {
             return 0;
         } else {
