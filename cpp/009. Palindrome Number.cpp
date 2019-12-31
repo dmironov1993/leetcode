cf2563c0e912cd5b279f4b40f99a205bf09a6959
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-12-31 17:51:58 -0600

    Update and rename 9. Palindrome Number.cpp to 009. Palindrome Number.cpp

--- cpp/009. Palindrome Number.cpp
+++ cpp/009. Palindrome Number.cpp
@@ -22,3 +22,23 @@ public:
 
 //Solution taken from here: 
 //https://www.youtube.com/watch?v=CumLEmaPero&list=PLES6U-jjEXscLKgGQkLglfLn7myfyGtjj&index=11
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int rev = 0;
+        int cur = x;
+        while (cur > 0) {
+            int d = cur % 10;
+            cur /= 10;
+            if (rev < INT_MAX / 10 || (rev == INT_MAX / 10 && d <= INT_MAX % 10)) {
+                rev = rev * 10 + d;
+            } else {
+                return false;
+            }            
+        }
+        return rev == x;
+    }
+};
