40bb34e52de8a98ca252e94d0cb9e7c4de806d8d
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2020-01-01 18:35:12 -0600

    Update 821. Shortest Distance to a Character.cpp

--- cpp/821. Shortest Distance to a Character.cpp
+++ cpp/821. Shortest Distance to a Character.cpp
@@ -63,3 +63,38 @@ public:
         return f;
     }
 };
+
+
+// Solution 1 taken from
+// https://www.youtube.com/watch?v=DYjgEOyiCXQ&list=PLES6U-jjEXscLKgGQkLglfLn7myfyGtjj&index=97
+
+class Solution {
+public:
+    vector<int> shortestToChar(const string s, char c) {
+        const int INF = (int)s.size();
+        vector<int> res(s.size(), INF);
+        int dist = INF;
+        for (int i = 0; i < (int)s.size(); i++) {
+            if (s[i] == c) {
+                dist = 0;
+            } else {
+                dist++;
+            } 
+            res[i] = dist;
+        }
+        dist = INF;
+        for (int i = (int)s.size() - 1; i >= 0; i--) {
+            if (s[i] == c) {
+                dist = 0;
+            } else {
+                dist++;
+            }
+            res[i] = min(res[i], dist);
+        }
+        return res;
+    }
+};
+
+// Solution 2 taken from
+// https://www.youtube.com/watch?v=DYjgEOyiCXQ&list=PLES6U-jjEXscLKgGQkLglfLn7myfyGtjj&index=97
+
